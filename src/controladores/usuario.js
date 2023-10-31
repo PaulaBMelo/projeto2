c49721d9ebfcc4511b4b4dd35c9932c54256f432
PaulaBMelo <paula.borges.tri@gmail.com>
2023-10-31 17:22:14 -0300

    atualizar usuario criado

--- src/controladores/usuario.js
+++ src/controladores/usuario.js
@@ -37,14 +37,49 @@ const cadastrarUsuario = async (req, res) => {
     }
 }
 
-//verificar no video se ele importou o intermediario pra cá.
-
 const detalharUsuario = async (req, res) => {
     return res.status(200).json(req.usuario);
+}
+
+const atualizarUsuario = async (req, res) => {
+    const { usuario } = req;
+
+    const { nome, email, senha } = req.body;
+
+    if (!nome || !email || !senha) {
+        return res.status(400).json({ mensagem: 'Todos os campos são obrigatórios' });
+    }
+
+    try {
+
+        const usuarioEncontrado = await query('select * from usuarios where email = $1', [email]);
+
+        if (usuarioEncontrado.rowCount > 0 && usuarioEncontrado.rows[0].id !== usuario.id) {
+            return res.status(400).json({ mensagem: 'O email já existe em cadastro' });
+        }
+
+        const senhaNova = await bcrypt.hash(senha, 10);
+
+        const queryAtualizacao = 'update usuarios set nome = $1, email = $2, senha = $3 where id = $4';
+        const queryParams = [nome, email, senhaNova, usuario.id];
+        const atualizacao = await query(queryAtualizacao, queryParams);
+
+        if (atualizacao.rowCount <= 0) {
+            return res.status(500).json({ mensagem: `Erro interno: ${error.message}` });
+        }
+
+        const { senha: _, ...cadastro } = atualizacao.rows[0];
+
+        return res.status(204).send(cadastro);
+
+    } catch (error) {
+        return res.status(500).json({ mensagem: `Erro: ${error.mensage}` });
+    }
 }
 
 
 module.exports = {
     cadastrarUsuario,
-    detalharUsuario
+    detalharUsuario,
+    atualizarUsuario
 }
--- src/rotas.js
+++ src/rotas.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { login } = require('./controladores/login');
-const { cadastrarUsuario, detalharUsuario } = require('./controladores/usuario');
+const { cadastrarUsuario, detalharUsuario, atualizarUsuario } = require('./controladores/usuario');
 const { filtrarAutenticacao } = require('./intermediarios/autenticacao');
 const rotas = express();
 
@@ -9,6 +9,7 @@ rotas.post('/login', login);
 
 rotas.use(filtrarAutenticacao);
 rotas.get('/usuario', detalharUsuario);
+rotas.put('/usuario', atualizarUsuario);
 
 module.exports = {
     rotas
